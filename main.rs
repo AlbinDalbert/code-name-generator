1278669b588653ea7962f08ec983bd50790ab911
Albin Dalbert <77275125+AlbinDalbert@users.noreply.github.com>
2022-11-02 14:05:39 +0100

    Update main.rs

--- main.rs
+++ main.rs
@@ -4,13 +4,11 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use rand::seq::SliceRandom;
 
-
 fn main() {
 
     let mut adjectives: Vec<String> = Vec::new();
     let mut nouns: Vec<String> = Vec::new();
     
-
     if let Ok(lines) = read_lines("adjectives.txt") {
         for line in lines {
             if let Ok(word) = line {
@@ -40,8 +38,6 @@ fn main() {
         let _s: String = read!("{}\n");
     }
 }
-
-
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
